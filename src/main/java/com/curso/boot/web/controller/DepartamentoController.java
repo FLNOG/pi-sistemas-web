b9cf519d3d1ba2af609b735f788b7ced469d955d
Felipe Nogueira <felipe@tecnbr.com.br>
2025-09-03 11:48:56 -0300

    feat: Adicona controllers

--- src/main/java/com/curso/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/curso/boot/web/controller/DepartamentoController.java
@@ -1,4 +1,20 @@
 package com.curso.boot.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/departamentos")
 public class DepartamentoController {
+
+    @GetMapping("/cadastrar")
+    public String cadastrar(){
+        return "/departamento/lista";
+    }
+
+    @GetMapping("/listar")
+    public String listar(){
+        return "/departamento/lista";
+    }
 }
